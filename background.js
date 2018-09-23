f0e6e5788a0969a88229800ebdfc2cce04ce4e37
Jason Wang <jason@wangjason.com>
2018-09-23 12:14:05 -0700

    scaffold

---
+++ background.js
@@ -0,0 +1,1 @@
+// Fill in what is running the background while extension is enabled (if needed)
---
+++ popup.js
@@ -0,0 +1,2 @@
+// Fill in what happens inside your popup.html
+// Example, clicking "Remember Page" will parse the current URL information and save it.
