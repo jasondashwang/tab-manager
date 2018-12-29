5863f7b63a87fee20be0f4e146ccdd995afcc18c
vtd125 <valeria_tiourina@brown.edu>
2018-12-28 23:10:44 -0500

    Dictionary for storage instead; trying to populate dropdown menu with existing folder names

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@
 // Example, clicking "Remember Page" will parse the current URL information and save it.
 
 var mySavedTabs = [];
+var myTabDict = {};
 
 // Collects the current tab's URL
 function simple(){
@@ -26,16 +27,51 @@ function myOpen(){
 
 // Saves the saved URL in a local list
 function saveCurrentTab(){
+    var url = document.getElementById("hehe").innerHTML
+    var folder = document.getElementById("newName").value
     if (!document.getElementById("openLink").disabled){
-        mySavedTabs.push(document.getElementById("hehe").innerHTML)
+        mySavedTabs.push(url)
     }
-    console.log(mySavedTabs);
+
+    // try to put it into the dictionary instead
+    if (createNewFolder(folder)){
+        console.log("folder created")
+        addTabToFolder(url, folder)
+    }
+
+    console.log(myTabDict)
 }
 
+// creates a new Folder for tabs
+function createNewFolder(key){
+    if (myTabDict[key]){
+        return false;
+    } else {
+        myTabDict[key] = []
+        return true;
+    }
+}
+
+function addTabToFolder(url, folder){
+    myTabDict[folder].push(url)
+}
+
+// use the myTabDict keys to populate a dropdown menu to select 
+// an existing folder to save the current URL to
+// function selectFolder(){
+//     var select = document.getElementById("example-select");
+//     for(folder in myTabDict) {
+//         select.options[select.options.length] = new Option(myTabDict[folder], folder);
+//     }
+//     var selected = select.options[select.selectedIndex].text;
+//     console.log(selected)
+//     return selected;
+// }
 
 // Activate all the buttons
 document.getElementById("clickMe").addEventListener('click',simple);
 document.getElementById("openLink").addEventListener('click',myOpen);
 document.getElementById("saveTab").addEventListener('click',saveCurrentTab);
+
 
 
